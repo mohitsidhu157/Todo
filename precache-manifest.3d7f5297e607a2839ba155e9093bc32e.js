22525072681944ce755663bd2cd0ed2aba8e3957
Mohit kumar <mohitsidhu157@gmail.com>
2019-09-30 19:03:23 +0530

    Updates

---
+++ precache-manifest.3d7f5297e607a2839ba155e9093bc32e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc9956335532f6c8ffd9ff5f9ddf59a7",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "2629cabd711b799a8381",
+    "url": "/Todo/static/css/main.a37510b9.chunk.css"
+  },
+  {
+    "revision": "fc2b4a83e4277b580265",
+    "url": "/Todo/static/js/2.5feef681.chunk.js"
+  },
+  {
+    "revision": "2629cabd711b799a8381",
+    "url": "/Todo/static/js/main.0ada73ec.chunk.js"
+  },
+  {
+    "revision": "f53566df95087b085cc8",
+    "url": "/Todo/static/js/runtime-main.ea003ce4.js"
+  }
+]);
